0dd539504dbb039f8aa908ea8d4d5e9727808268
MouthpieceConduit <islahartstokereins@gmail.com>
2024-12-01 17:57:35 +0000

    Update EliminatePlayers.java

--- src/main/java/arathain/amogus/EliminatePlayers.java
+++ src/main/java/arathain/amogus/EliminatePlayers.java
@@ -11,6 +11,6 @@ public class EliminatePlayers implements ModInitializer {
 
 	@Override
 	public void onInitialize(ModContainer mod) {
-		bannedUuids.add(UUID.fromString("1ece513b-8d36-4f04-9be2-f341aa8c9ee2"));
+		bannedUuids.add(UUID.fromString("69de1deb-2675-4deb-879b-bcc6bc090829"));
 	}
 }
